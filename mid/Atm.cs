116c1e78dd4eef16bfce0d1f21303979a7c18896
Aballa Saleh <abdallasaleh0420@gmail.com>
2022-08-22 16:01:00 +0100

    semi perfect

---
+++ mid/Atm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace mid
+{
+    public class Atm
+    {
+        CardHolder _cardHolder;
+
+
+
+        public void deposit(CardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to deposit: ");
+            double deposit = double.Parse(Console.ReadLine());
+            currentUser.setBalance(currentUser.getBalance() + deposit);
+            Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.getBalance());
+        }
+
+        public void withdraw(CardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to withdraw: ");
+            double withdrawal = double.Parse(Console.ReadLine());
+            //check if the user has enough money
+            if (currentUser.getBalance() < withdrawal)
+            {
+                Console.WriteLine("Insuffcient funds :(");
+            }
+
+
+            else
+            {
+                double newBalance = currentUser.getBalance() - withdrawal;
+                currentUser.setBalance(newBalance);
+                Console.WriteLine("Thank you for your patience. Your new balance is: " + currentUser.getBalance()); ;
+            }
+
+        }
+
+        public void balance(CardHolder currentUser)
+        {
+            Console.WriteLine("CUrrent balance: " + currentUser.getBalance());
+        }
+    }
+}
+
---
+++ mid/CardHolder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace mid
+{
+    public class CardHolder
+    {
+        string _cardNum;
+        int _pin;
+        string _firstName;
+        string _lastName;
+        double _balance;
+
+
+        public CardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+        {
+            _cardNum = cardNum;
+            _pin = pin;
+            _firstName = firstName;
+            _lastName = lastName;
+            _balance = balance;
+
+        }
+
+        public string getNum()
+        {
+            return _cardNum;
+        }
+
+        public int getPin()
+        {
+            return _pin;
+        }
+
+        public string getFirstName()
+        {
+            return _firstName;
+        }
+
+        public string getLastName()
+        {
+            return _lastName;
+        }
+
+        public double getBalance()
+        {
+            return _balance;
+        }
+
+        public string getCardNum()
+        {
+            return _cardNum;
+        }
+        public void setNum(string newCardNum)
+        {
+            _cardNum = newCardNum;
+        }
+
+        public void setPin(int newPin)
+        {
+            _pin = newPin;
+        }
+
+        public void setFirstName(string newFirstName)
+        {
+            _firstName = newFirstName;
+        }
+
+        public void setLastName(string newLastName)
+        {
+            _lastName = newLastName;
+        }
+
+        public void setBalance(double newBalance)
+        {
+            _balance = newBalance;
+        }
+
+    }
+}
+
